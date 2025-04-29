c388773903fc3f0d925f94fa1b6b4c8a279683b6
abdule18 <101951085+abdule18@users.noreply.github.com>
2025-04-28 22:07:18 -0400

    I have updated everything

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -10,12 +10,15 @@ public class Main {
     private static ArrayList<Transaction> transactions = new ArrayList<Transaction>();
     private static String filename;
     private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         filename = "transactions.csv";
+        createFileIfNotExists();
+        getListedTransaction();
         homeScreen();
     }
 
-    private static void homeScreen(){
+    private static void homeScreen() {
         System.out.println("Hello, Welcome to Java CLI App.");
 
         String userPromptOptions = "Please choose an option you would like to move forward with:\n" +
@@ -29,7 +32,7 @@ public class Main {
             System.out.println(userPromptOptions);
             option = scanner.nextLine();
 
-            if (option.equalsIgnoreCase("D")){
+            if (option.equalsIgnoreCase("D")) {
                 addDeposit();
             } else if (option.equalsIgnoreCase("P")) {
                 makePayment();
@@ -39,11 +42,8 @@ public class Main {
         } while (!option.equalsIgnoreCase("x"));
     }
 
-    private static void addDeposit(){
-//    FileWriter fileWriter = null;
-//    try {
-//        fileWriter = new FileWriter("deposit_Info.csv");
-//        BufferedWriter bufWriter = new BufferedWriter(fileWriter);
+    private static void addDeposit() {
+
         LocalDateTime date = LocalDateTime.now();
         LocalTime time = LocalTime.now();
 
@@ -60,40 +60,71 @@ public class Main {
         Transaction newTransaction = new Transaction(date, time, description, vendor, amount);
 
         transactions.add(newTransaction);
-        System.out.println(date + "|" + time + "|" +description + "|" + vendor + "|-" + amount);
-
-
-}
-
-    private static void makePayment(){
-        LocalDateTime date = LocalDateTime.now();
-        LocalTime time = LocalTime.now();
+        System.out.println(newTransaction.getFormattedBookTextHeader());
         FileWriter fileWriter = null;
         try {
-            fileWriter = new FileWriter("debit_info.csv");
+            fileWriter = new FileWriter(filename, true);
             BufferedWriter bufWriter = new BufferedWriter(fileWriter);
 
-
-            System.out.print("Enter a description: ");
-            String description = scanner.nextLine();
-
-            System.out.print("Who are you making the payment to: ");
-            String vendor = scanner.nextLine();
-
-            System.out.print("Amount you would like to pay: ");
-            String amount = scanner.nextLine();
-
-
-            bufWriter.write(date + "|" + time + "|" + description + "|" + vendor + "|" + amount);
+            bufWriter.write(newTransaction.getEncodedText());
+            bufWriter.newLine();
             bufWriter.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
+        System.out.println(newTransaction.getEncodedText());
 
-}
+    }
 
-    private static void displayLedger(){
+    private static void createFileIfNotExists() {
+        File file = new File(filename);
+
+        try {
+            if (file.createNewFile()) {
+                System.out.println("transactions.csv created.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred while creating the file.");
+        }
+    }
+
+    private static void makePayment() {
+        LocalDateTime date = LocalDateTime.now();
+        LocalTime time = LocalTime.now();
+
+        System.out.print("Enter a description: ");
+        String description = scanner.nextLine();
+
+        System.out.print("Enter the vendor: ");
+        String vendor = scanner.nextLine();
+
+        System.out.print("Amount you would like to pay: ");
+        double amount = scanner.nextDouble();
+        scanner.nextLine();
+
+        Transaction newTransaction = new Transaction(date, time, description, vendor, -amount);
+
+        transactions.add(newTransaction);
+
+        FileWriter fileWriter = null;
+        try {
+            fileWriter = new FileWriter(filename, true);
+            BufferedWriter bufWriter = new BufferedWriter(fileWriter);
+
+            bufWriter.write(newTransaction.getEncodedText());
+            bufWriter.newLine();
+            bufWriter.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(newTransaction.getEncodedText());
+
+
+    }
+
+    private static void displayLedger() {
 
         String userPromptOptions = "Please choose an option you would like to move forward with:\n" +
                 "A - All\n" +
@@ -107,7 +138,7 @@ public class Main {
             System.out.println(userPromptOptions);
             option = scanner.nextLine();
 
-            if (option.equalsIgnoreCase("A")){
+            if (option.equalsIgnoreCase("A")) {
                 displayAllEntries();
             } else if (option.equalsIgnoreCase("D")) {
                 displayEntriesDeposits();
@@ -119,38 +150,37 @@ public class Main {
         } while (!option.equalsIgnoreCase("H"));
     }
 
-    private static void displayAllEntries(){
+    private static void displayAllEntries() {
         System.out.println("Display All Entries pending......");
     }
 
-    private static void displayEntriesDeposits(){
+    private static void displayEntriesDeposits() {
         System.out.println("Deposits Entries pending......");
     }
 
-    private static void displayNegativePayments(){
+    private static void displayNegativePayments() {
         System.out.println("Negative Payments pending......");
     }
 
-    private static void repotOptions(){
+    private static void repotOptions() {
         System.out.println("Reports pending......");
     }
 
-    private static Transaction[] getListedTransaction(){
+    private static ArrayList<Transaction> getListedTransaction() {
         FileReader fr = null;
         try {
             fr = new FileReader(filename);
 
             BufferedReader reader = new BufferedReader(fr);
 
-            ArrayList<Transaction> transactions = new ArrayList<Transaction>();
+            transactions.clear();
             String dataString;
 
-            while ((dataString = reader.readLine()) !=  null){
+            while ((dataString = reader.readLine()) != null) {
                 Transaction transaction = getTransactionFromEncodedString(dataString);
                 transactions.add(transaction);
             }
             reader.close();
-            return transactions;
 
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
@@ -158,9 +188,11 @@ public class Main {
             throw new RuntimeException(e);
         }
 
+        return transactions;
+
     }
 
-    public static Transaction getTransactionFromEncodedString(String dataString){
+    public static Transaction getTransactionFromEncodedString(String dataString) {
         String[] tokens = dataString.split("\\|");
 
         LocalDateTime date = LocalDateTime.parse(tokens[0]);
@@ -171,12 +203,4 @@ public class Main {
 
         return new Transaction(date, time, description, vendor, amount);
     }
-
-//    private static ArrayList<Transaction> getTransaction(){
-//        ArrayList<Transaction> transactions = new ArrayList<Transaction>();
-//
-//        transactions.add();
-//
-//        return transactions;
-//    }
 }
--- src/main/java/com/pluralsight/Transaction.java
+++ src/main/java/com/pluralsight/Transaction.java
@@ -18,45 +18,7 @@ public class Transaction {
         this.amount = amount;
     }
 
-    public LocalDateTime getDate() {
-        return date;
-    }
 
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-    public LocalTime getTime() {
-        return time;
-    }
-
-    public void setTime(LocalTime time) {
-        this.time = time;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getVendor() {
-        return vendor;
-    }
-
-    public void setVendor(String vendor) {
-        this.vendor = vendor;
-    }
-
-    public double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(double amount) {
-        this.amount = amount;
-    }
 
     public String getEncodedText(){
         return this.date + "|" + this.time + "|" + this.description + "|" + this.vendor + "|" + this.amount;
